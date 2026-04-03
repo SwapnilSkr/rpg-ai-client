82470118fc26c5c98a4b7013424f2393cc47e4ab
SwapnilSkr <swapnilmkab@gmail.com>
2026-04-03 20:46:27 +0530

    added routes and app main entry point

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -1,18 +1,48 @@
 import 'package:go_router/go_router.dart';
 import 'package:everlore/screens/auth_screen.dart';
-import 'package:everlore/screens/home_screen.dart';
+import 'package:everlore/features/home/presentation/home_screen.dart';
+import 'package:everlore/features/play/presentation/play_screen.dart';
+import 'package:everlore/features/chronicle/presentation/chronicle_screen.dart';
+import 'package:everlore/features/templates/presentation/browse_screen.dart';
+import 'package:everlore/features/templates/presentation/template_detail_screen.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
       name: 'home',
-      builder: (context, state) => const MyHomePage(title: 'Everlore'),
+      builder: (context, state) => const HomeScreen(),
     ),
     GoRoute(
       path: '/auth',
       name: 'auth',
       builder: (context, state) => const AuthScreen(title: 'Everlore'),
+    ),
+    GoRoute(
+      path: '/play/:instanceId',
+      name: 'play',
+      builder: (context, state) => PlayScreen(
+        instanceId: state.pathParameters['instanceId']!,
+      ),
+    ),
+    GoRoute(
+      path: '/chronicle/:instanceId',
+      name: 'chronicle',
+      builder: (context, state) => ChronicleScreen(
+        instanceId: state.pathParameters['instanceId']!,
+      ),
+    ),
+    GoRoute(
+      path: '/templates',
+      name: 'templates',
+      builder: (context, state) => const BrowseTemplatesScreen(),
+    ),
+    GoRoute(
+      path: '/templates/:templateId',
+      name: 'template_detail',
+      builder: (context, state) => TemplateDetailScreen(
+        templateId: state.pathParameters['templateId']!,
+      ),
     ),
   ],
 );
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'app_routes.dart';
+import 'app/theme/nexus_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const EverloreApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class EverloreApp extends StatelessWidget {
+  const EverloreApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      title: 'Everlore',
       debugShowCheckedModeBanner: false,
+      theme: NexusTheme.dark,
       routerConfig: router,
     );
   }
